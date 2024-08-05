397a560f40038e524f98da52ec0c4243dea62776
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-05 19:09:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const to_do_input =  document.querySelector('#todo');
+const list = document.querySelector('#list');
+const to_do_form = document.querySelector('#to_do_form');
+const sc_feedback = document.querySelector('#sc_feedback');
+const heading = document.querySelector('#heading');
+
+to_do_form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let task = to_do_input.value;
+    console.log(task);
+    addTaskToDOM(task);
+    removeValue(to_do_input);
+    screenReaderFeedback(task);
+
+});
+
+list.addEventListener('click', function(event) {
+    if (hasClassName(event.target, 'delete_task')) {
+        const li = event.target.closest('li');
+        const taskName = event.target.previousElementSibling.textContent;
+        deleteTask(li);
+        screenReaderFeedback(taskName, 'removed');
+        moveFocus(heading);
+
+    }
+})
+
+function deleteTask(theTarget) {
+    list.removeChild(theTarget);
+}
+
+function moveFocus(element) {
+    element.focus();
+
+}
+
+function addTaskToDOM(task) {
+    let newID = generateID();
+    let taskItem = createElement('li','', list, ['class', 'task']);
+    let theCheckbox = createElement('input', null, taskItem, ['type', 'checkbox']);
+    let label = createElement('label', task, taskItem, ['for', newID]);
+    theCheckbox.setAttribute('id', newID);
+    let deleteButton = createElement('button', 'Delete Task', taskItem, ['class', 'delete_task']);
+}
+
+function removeValue(input) {
+    input.value = '';
+}
+
+function screenReaderFeedback(task, feedback='added') {
+    sc_feedback.textContent = `${task} ${feedback}.`; 
+}
+
+function createElement(tagName, textNode, parent, attribute = null) {
+    let node = document.createElement(tagName);
+    if(textNode != null) {
+        let customTextNode = document.createTextNode(textNode);
+        node.appendChild(customTextNode);
+    }
+
+    if(attribute != null) {
+        node.setAttribute(attribute[0], attribute[1]);
+    }
+
+    parent.appendChild(node);
+    return node;
+}
+
+function generateID() {
+    let idPrefix = 'task_num_';
+    let tasks = document.querySelectorAll('#list>li');
+    if (tasks.length == 0) {
+        return `${idPrefix}0`
+    }
+
+    return idPrefix + tasks.length;
+}
+
+function hasClassName(element, className) {
+    if(element.classList.contains(className)) {
+        return true;
+    }
+    return false;
+}
